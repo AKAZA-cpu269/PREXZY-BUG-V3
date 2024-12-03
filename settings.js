7051ae6595ecb510c3a090b5859f98d94c82c325
AKAZA-cpu269 <mideo1375@gmail.com>
2024-12-03 16:37:53 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2347063956321"
-global.ownername = "Precious Ayomide👑"
+global.ownernomer = "2347013792734"
+global.ownername = "REAL ZION BUG"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
